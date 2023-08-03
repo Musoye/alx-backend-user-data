3a5b64d051cf9d4d39197dbb10399ed206b2ca67
Musoye <oyebamijimustapha44@gmail.com>
2023-08-03 21:31:20 +0100

    starting

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+"""Deal about filtering data"""
+
+from typing import List
+import logging
+import re
+from os import getenv
+import mysql.connector
+
+
+def filter_datum(fields: List[str], redaction: str,
+                 message: str, separator: str) -> str:
+    '''
+    function called filter_datum that
+    returns the log message obfuscated
+    '''
+    for field in fields:
+        mes = re.sub(field+'=.*?'+separator,
+                         field+'='+redaction+separator, message)
+    return mes
