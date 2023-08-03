d6d6ee3fd4c5056bdab2349a00c93fd2f4351c17
Musoye <oyebamijimustapha44@gmail.com>
2023-08-03 21:50:00 +0100

    Create Logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -8,6 +8,9 @@ from os import getenv
 import mysql.connector
 
 
+PII_FIELDS = ('email', 'name', 'ssn', 'password', 'phone')
+
+
 def filter_datum(fields: List[str], redaction: str,
                  message: str, separator: str) -> str:
     '''
@@ -18,3 +21,36 @@ def filter_datum(fields: List[str], redaction: str,
         mes = re.sub(field+'=.*?'+separator,
                      field+'='+redaction+separator, message)
     return mes
+
+
+class RedactingFormatter(logging.Formatter):
+    """ Redacting Formatter class
+        """
+
+    REDACTION = "***"
+    FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
+    SEPARATOR = ";"
+
+    def __init__(self):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+
+    def format(self, record: logging.LogRecord) -> str:
+        '''
+        format method to filter values in
+        incoming log records using filter_datum
+        '''
+        message = super(RedactingFormatter, self).format(record)
+        redact = filter_datum(self.fields, self.REDACTION,
+                              message, self.SEPARATOR)
+
+
+def get_logger() -> logging.Logger:
+    '''get logging function'''
+    logger = logging.getLogger("user_data")
+    logger.propagate = False
+    logger.setLevel(logging.INFO)
+    handler = logger.StreamHandler()
+    fmt = RedactingFormatter(PII_FIELDS)
+    handler.setFormatter(fmt)
+    logger.addHandler(handler)
+    return logger
