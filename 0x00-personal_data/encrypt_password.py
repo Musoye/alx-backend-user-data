f7a7d10a217cf758f02fc8bd29f2d2e90c0a67a4
Musoye <oyebamijimustapha44@gmail.com>
2023-08-03 22:31:28 +0100

    check if password is va,id

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -10,3 +10,10 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed = bcrypt.hashpw(password.encode(), salt)
     return hashed
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    '''check if hashed password is the same as paaa'''
+    if bcrypt.checkpw(password.encode(), hashed_password):
+        return True
+    return False
