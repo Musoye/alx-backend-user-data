d88ecf8bf91e23c5f9a8508713732cd04c16735f
Musoye <oyebamijimustapha44@gmail.com>
2023-08-03 21:55:14 +0100

    create logger

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -31,8 +31,9 @@ class RedactingFormatter(logging.Formatter):
     FORMAT = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: %(message)s"
     SEPARATOR = ";"
 
-    def __init__(self):
+    def __init__(self, fields: List[str]):
         super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
 
     def format(self, record: logging.LogRecord) -> str:
         '''
@@ -42,6 +43,7 @@ class RedactingFormatter(logging.Formatter):
         message = super(RedactingFormatter, self).format(record)
         redact = filter_datum(self.fields, self.REDACTION,
                               message, self.SEPARATOR)
+        return redact
 
 
 def get_logger() -> logging.Logger:
