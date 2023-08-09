49d2e99cbb3b11043182b5832312c0fdebb4eb86
Musoye <oyebamijimustapha44@gmail.com>
2023-08-09 02:01:52 +0100

    Basic - Base64 decode

--- 0x01-Basic_authentication/api/v1/auth/basic_auth.py
+++ 0x01-Basic_authentication/api/v1/auth/basic_auth.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 """Basic Auth using AUTH"""
 from api.v1.auth.auth import Auth
+import base64
 
 
 class BasicAuth(Auth):
@@ -14,3 +15,15 @@ class BasicAuth(Auth):
         if not authorization_header.startswith("Basic "):
             return None
         return authorization_header.split()[1]
+
+    def decode_base64_authorization_header(self, base64_authorization_header: str) -> str:
+        """decode base63 authorization"""
+        if base64_authorization_header is None:
+            return None
+        if type(base64_authorization_header) is not str:
+            return None
+        try:
+            bas = base64_authorization_header.encode('utf-8')
+            return base64.b64decode(bas).decode('utf-8')
+        except BaseException:
+            return None
